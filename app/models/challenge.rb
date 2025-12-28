101dd2f8f3b3945fc255f981a6dd9b2492858476
jean toneli <jeantoneli@gmail.com>
2025-12-28 22:50:02 +0000

    chat + message models + dependents - fixing

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,5 +1,4 @@
 class Challenge < ApplicationRecord
   validates :name, :module, :content, presence: true
   has_many :chats, dependent: :destroy
-  has_many :messages, dependent: :destroy
 end
--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,5 @@
 class Chat < ApplicationRecord
   belongs_to :challenge
   belongs_to :user
+  has_many :messages, dependent: :destroy
 end
