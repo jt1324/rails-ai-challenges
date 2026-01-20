4b035f3c86e875362dba0d6a43e5787bbdef8ed8
jean toneli <jeantoneli@gmail.com>
2026-01-20 22:57:50 +0000

    adding sidebar with javascript

---
+++ app/javascript/controllers/toggle_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="toggle"
+export default class extends Controller {
+  static targets = ["hideable", "show"]
+  connect() {
+    console.log("Hello from toggle_controller.js")
+    console.log(this.hideableTarget);
+    console.log(this.showTarget);
+  }
+
+  call(event) {
+    event.preventDefault()
+    if (this.hideableTarget.classList.contains("d-none")) {
+      this.hideableTarget.classList.remove("d-none")
+      this.showTarget.classList.add("d-none")
+    } else {
+      this.hideableTarget.classList.add("d-none")
+      this.showTarget.classList.remove("d-none")
+    }
+  }
+
+}
